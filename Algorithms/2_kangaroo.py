fc2a70a0034cff95e402fe17380b88a120d04814
meda123 <mrt62@cornell.edu>
2017-07-14 19:44:46 -0700

    Challenge completed. Need to add explanation.

--- Algorithms/2_kangaroo.py
+++ Algorithms/2_kangaroo.py
@@ -25,6 +25,10 @@ import sys
 
 def kangaroo(x1, v1, x2, v2):
     # Complete this function
+     if (v1 > v2) and ((x1 - x2) % (v1 - v2)) == 0:
+       return"YES"
+    else:
+       return"NO"
 
 x1, v1, x2, v2 = raw_input().strip().split(' ')
 x1, v1, x2, v2 = [int(x1), int(v1), int(x2), int(v2)]
