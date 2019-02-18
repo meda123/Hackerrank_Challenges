ba92a824eabb8e5202bf5a48352cab2ab455d505
meda123 <mrt62@cornell.edu>
2019-02-18 07:24:51 -0500

    Completed condition

--- Python/Conditional.py
+++ Python/Conditional.py
@@ -1,36 +1,15 @@
-import sys
 
+#Challenge: https://www.hackerrank.com/challenges/py-if-else/problem
+
+import sys
 
 N = int(raw_input().strip())
 
-if N % 2 == 0:
-    if N >= 2 and N <=5:
-        #print "Not weird"
-        sys.stdout.write("Not weird")
-    elif N >= 6 and N <=20:
-        #print "Weird"
-        sys.stdout.write("Weird")
-    elif N > 20:
-        #print "Not Weird"
-        sys.stdout.write("Not Weird")
-
-if N % 2 == 1:
-
-#else:
-    #print "Weird"
+if N % 2 == 1;
     sys.stdout.write("Weird")
-
-
-
-if N % 2 == 1:
-    #print(w)
-    sys.stdout.write("Weird")
-elif N % 2 == 0 and (N>=2 and N<5):
-    #print(nw)
-    sys.stdout.write("Not weird")
+elif N % 2 and == 0 and (N>=2 and N<5):
+    sys.out.write("Not Weird")
 elif N % 2 == 0 and (N>=6 and N<=20):
-    #print(w)
-    sys.stdout.write("Weird")
+    sys.out.write("Weird")
 elif N % 2 == 0 and (N>20):
-    #print(nw)
-    sys.stdout.write("Not weird")
+    sys.out.write("Not Weird")
