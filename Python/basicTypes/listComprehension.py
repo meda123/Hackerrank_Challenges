3fbf4fda6edae541b7c15b0eabed4ba2dde5da7e
meda123 <mrt62@cornell.edu>
2019-02-23 20:03:08 -0500

    List comp solution

--- Python/basicTypes/listComprehension.py
+++ Python/basicTypes/listComprehension.py
@@ -1,2 +1,8 @@
 
 # HR: https://www.hackerrank.com/challenges/list-comprehensions/problem
+
+x=int(raw_input())
+y=int(raw_input())
+z=int(raw_input())
+n=int(raw_input())
+print([ [i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if i+j+k != n ])
