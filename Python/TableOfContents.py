810031c5bc275a81decd9a82e1b3303fabbdcf71
meda123 <mrt62@cornell.edu>
2019-03-06 06:54:28 -0500

    Added comments to list comprehension and list

--- Python/TableOfContents.py
+++ Python/TableOfContents.py
@@ -1,13 +1,20 @@
 
 # CATEGORIES
 
+
 #Introduction: Week Feb 25th
 # Basic Data Types: Week of March 4th
+    # findingPercentages: Reviewed
+    # listComprehension: Reviwed
+    # lists
+    # nestedLists
+    # runnerUpScore 
+
 # Strings:
 # Sets:
 # Math:
 # Intertols:
-# Collections: 
+# Collections:
 # Date and Time:
 # Errors and Exceptions
 # Classes
--- Python/basicTypes/listComprehension.py
+++ Python/basicTypes/listComprehension.py
@@ -1,8 +1,30 @@
 
 # HR: https://www.hackerrank.com/challenges/list-comprehensions/problem
+'''
+Sample Input:
+1
+1
+1
+2
+
+Sample Output:
+[[0, 0, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0], [1, 1, 1]]
+
+Cocenpt:
+List comprehensions are an elegant way to build a list without having to use
+different for loops to append values one by one
+
+Explantion:
+You are given three integers x,y,z representing the dimensions of a cuboid
+along with an integer  N. You have to print a list of all possible coordinates
+given by  on a 3D grid where the sum of  is not equal to N .
+
+'''
+
 
 x=int(raw_input())
 y=int(raw_input())
 z=int(raw_input())
 n=int(raw_input())
+
 print([ [i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if i+j+k != n ])
