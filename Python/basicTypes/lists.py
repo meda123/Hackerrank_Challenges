a69795a41555ed5b3d8b78813ea564a5547a94b5
meda123 <mrt62@cornell.edu>
2019-03-09 10:27:41 -0500

    Solution to lists

--- Python/basicTypes/lists.py
+++ Python/basicTypes/lists.py
@@ -25,4 +25,32 @@ Output:
 '''
 
 if __name__ == '__main__':
+
+    list = []
     N = int(raw_input())
+
+    while N > 0:
+        i = raw_input.split()
+
+        if i[0] == "insert":
+            list.insert(int(i[1])), int(i[2]))
+
+        elif i[0] == "remove":
+            list.remove(int(i[1]))
+
+        elif i[0] == "append":
+            list.append(int(i[1]))
+
+        elif i[0] == "sort":
+            list.sort()
+
+        elif i[0] == "reverse":
+            list.reverse()
+
+        elif i[0] == "pop":
+            list.pop()
+
+        elif i[0] == "print"
+            print(list)
+
+        N -=1
