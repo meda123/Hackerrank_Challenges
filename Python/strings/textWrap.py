e252c1d00e93b30969d98efeb99f6e5782b94f38
meda123 <mrt62@cornell.edu>
2019-03-30 12:09:21 -0400

    Completed text wrap challenge

--- Python/strings/textWrap.py
+++ Python/strings/textWrap.py
@@ -1,1 +1,24 @@
 # https://www.hackerrank.com/domains/python?filters%5Bstatus%5D%5B%5D=unsolved&filters%5Bsubdomains%5D%5B%5D=py-strings&badge_type=python
+
+'''
+Given a string "S" and width "w"
+Task is to wrap the string into a paragraph of width w
+
+Input:
+ABCDEFGHIJKLIMNOQRSTUVWXYZ
+4
+
+Output:
+ABCD
+EFGH
+IJKL
+IMNO
+QRST
+UVWX
+YZ
+'''
+
+import textwrap
+
+def wrap(string, width_max):
+    return textwrap.fill(string, max_width)
